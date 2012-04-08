2fb22c7bf3f4911ab0fd3c560b1971e5233823aa
RaNaN <Mast3rranan@hotmail.de>
2012-04-08 23:51:00 +0200

    multiprocessing, more printed stats

--- game/Statistics.py
+++ game/Statistics.py
@@ -2,7 +2,7 @@
 
 from time import time
 
-from utils import avg, mean
+from utils import avg, median
 
 from constants import STATE
 
@@ -11,8 +11,33 @@ GAME_CT = 1000
 class Statistics:
     def __init__(self, game):
         self.game = game
-
         self.results = []
+
+    def spawn(self, n=GAME_CT):
+        # Fallback on import error or single core
+        try:
+            from multiprocessing import Process, Manager, cpu_count
+        except ImportError:
+            return self.run(n)
+
+        # For low n multiprocessing does not gain much speed up
+        if cpu_count() <= 1 or n < 500:
+            return self.run(n)
+
+        m = Manager()
+        self.results = m.list()
+        procs = []
+        load = [n // cpu_count()] * cpu_count()
+
+        # add the rest from division to last cpu
+        load[-1] += n % cpu_count()
+
+        for count in load:
+            proc = Process(target=self.run, args=(count,))
+            proc.start()
+            procs.append(proc)
+
+        [p.join() for p in procs]
 
     def run(self, n=GAME_CT):
         for i in range(n):
@@ -24,7 +49,7 @@ class Statistics:
 
 
     def __str__(self):
-        s = "%s\nPlays: %s" % (self.game.get_name(), len(self.results))
+        s = "%s\n%s\n\nPlays: %s" % (self.game.get_name(), self.game.get_game_info(), len(self.results))
 
         data = zip(*self.results)
 
@@ -34,7 +59,7 @@ class Statistics:
 
         s += "\n\tTurns\tTime"
 
-        for name, f in (("Minimum", min), ("Avg.", avg), ("Mean", mean), ("Maximum", max), ("Total", sum)):
+        for name, f in (("Minimum", min), ("Avg.", avg), ("Median", median), ("Maximum", max), ("Total", sum)):
             s += "\n%s\t%s\t%.2f" % (name, f(data[1]), f(data[2]))
 
         return s
