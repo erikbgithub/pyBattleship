b03b428514e647dacced2191265bb9aefe4a6e35
sholvar <erik.bernoth@googlemail.com>
2012-04-09 02:53:30 +0300

    some comments reviewed

--- distributors/AbstractDistributor.py
+++ distributors/AbstractDistributor.py
@@ -2,11 +2,13 @@
 
 class AbstractDistributor(object):
     """
-    Abstract class for all distribution algorithm, classes that set up the game fields with their ships.
+    Abstract class for all ship distributoring algorithms and
+    classes that set up the game fields with their ships.
     """
 
     def set_ships(self, board):
-        """ All ships has to be set on the board.
+        """
+        All ships have to be set on the board.
         use board.add_ship for this purpose
 
         :param board: board instance
